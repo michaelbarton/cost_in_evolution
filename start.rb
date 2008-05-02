4e88ce23485c2f8769f66e740c908b8e283957bd
Michael Barton <mail@michaelbarton.me.uk>
2008-05-02 17:45:53 +0100

    Updated loading of application server log file
    Changed from log_file to www_log
    Updated start.rb accordingly

--- start.rb
+++ start.rb
@@ -2,5 +2,5 @@ require File.dirname(__FILE__) + '/config/environment.rb'
 
 config = YAML::load(File.open(File.dirname(__FILE__) + '/config/config.yml'))
 
-Ramaze::Log.loggers << Ramaze::Informer.new(config['log_file'])
+Ramaze::Log.loggers << Ramaze::Informer.new(config['www_log'])
 Ramaze.start config['ramaze']
