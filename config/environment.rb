7ad4d708d8c0defa3c203e827a73d89d539717f1
Michael Barton <mail@michaelbarton.me.uk>
2008-05-02 16:05:43 +0100

    Added database configuration requirements.
    Added activerecord gem requirement to environment.rb
    Added activerecord loading based on database configuration
    Added ignore line to .gitignore to include database configuration yaml files

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'ramaze'
 require 'yaml'
+require 'activerecord'
+
+ActiveRecord::Base.establish_connection(YAML::load(File.open(File.dirname(__FILE__) + '/database.yml')))
 
 Dir.glob(File.dirname(__FILE__) + '/../controller/*.rb') {|file| require file}
 Dir.glob(File.dirname(__FILE__) + '/../model/*.rb') {|file| require file}
