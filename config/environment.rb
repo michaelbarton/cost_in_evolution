1022606f534de7da347b1fd641c26cadecb7b951
Michael Barton <mail@michaelbarton.me.uk>
2008-05-02 16:56:49 +0100

    Added active record logging in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require 'yaml'
 require 'activerecord'
 
 ActiveRecord::Base.establish_connection(YAML::load(File.open(File.dirname(__FILE__) + '/database.yml')))
+ActiveRecord::Base.logger = Logger.new 'log/db.log'
 
 Dir.glob(File.dirname(__FILE__) + '/../controller/*.rb') {|file| require file}
 Dir.glob(File.dirname(__FILE__) + '/../model/*.rb') {|file| require file}
