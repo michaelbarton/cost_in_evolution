94b23defe925d5e1202ac47f4f95b0971c23f071
Michael Barton <mail@michaelbarton.me.uk>
2008-05-03 20:02:21 +0100

    Added helper method to model to generate version string

--- model/project.rb
+++ model/project.rb
@@ -5,4 +5,8 @@ class Project < DataMapper::Base
   property :major_version, :integer
   property :minor_version, :integer
   property :tiny_version,  :integer
+
+  def version
+    "#{self.major_version}.#{self.minor_version}.#{self.tiny_version}"
+  end
 end
