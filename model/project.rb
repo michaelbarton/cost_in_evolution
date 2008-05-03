bb0e655f501626aced9d6e713ada470a411b04ee
Michael Barton <mail@michaelbarton.me.uk>
2008-05-03 18:12:34 +0100

    Renamed description property to summary

--- model/project.rb
+++ model/project.rb
@@ -1,6 +1,6 @@
 class Project < DataMapper::Base
   property :title,         :string
-  property :body,          :text
+  property :summary,       :text
   property :last_modified, :datetime
   property :major_version, :integer
   property :minor_version, :integer
