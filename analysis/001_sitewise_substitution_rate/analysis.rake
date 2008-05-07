9dacadb4e1d1794c39b75a10d6755b461de55628
Michael Barton <mail@michaelbarton.me.uk>
2008-05-07 16:06:48 +0100

    M001 Added task and model methods to load yeast alignment data
    Task parses through gzipped alignment file and separates each entry based on the number of entries identifier
    Passes each entry to the Alignment model
    Alignment model checks that there is a yeast gene in the alignment and that a corresponding entry exists in the database
    Then stores entry in the database

--- analysis/001_sitewise_substitution_rate/analysis.rake
+++ analysis/001_sitewise_substitution_rate/analysis.rake
@@ -20,6 +20,21 @@ namespace '001' do
     Alignment.delete_all
   end
 
+  desc 'Loads the sequence alignment data'
+  task :load_alignment_data => ['clear_alignment_data'] do
+    file_gz = File.dirname(__FILE__) + '/data/yeast_alignments.txt.gz'
+    Zlib::GzipReader.open(file_gz) do |file|
+      entry = file.readline
+      file.each_line do |line|
+        if line =~ /\d\s\d+/ 
+          Alignment.create_from_alignment(entry.strip)
+          entry = line
+        end
+        entry += line
+      end
+    end
+  end
+
   desc 'Clears all data, and repeats milestone 001 analysis'
   task :rebuild => [
     'load_sequence_data'
--- model/alignment.rb
+++ model/alignment.rb
@@ -3,4 +3,46 @@ class Alignment < DataMapper::Base
   property :alignment,  :text
   property :gene_count, :integer
   property :length,     :integer
+
+  def self.create_from_alignment(entry)
+ 
+    cerevisiae_gene = self.find_yeast_gene_name(entry)
+    if cerevisiae_gene.nil?
+      return
+    end
+
+    gene = Gene.first(:name => cerevisiae_gene)
+    if gene.nil?
+      return 
+    end
+
+    stats = self.alignment_statistics(entry)
+
+    Alignment.create(
+      :gene_id     => gene.id,
+      :alignment   => self.remove_first_line(entry),
+      :gene_count  => stats[:count],
+      :length      => stats[:length])
+  end
+
+  private
+  
+  def self.find_yeast_gene_name(alignment)
+    match = /F(Y[A-Z]{2}\d{3}[CW])/.match(alignment)
+    if match
+      match[1]
+    else
+      nil
+    end
+  end
+
+  def self.alignment_statistics(alignment)
+    count,length = /(\d+)\s+(\d+)/.match(alignment).captures
+    {:count => count.to_i,:length => length.to_i}
+  end
+
+  def self.remove_first_line(entry)
+    t = entry.split(/[\n\r]/)
+    t[1,t.length] * '\n'
+  end
 end
