d70cd399b2191e052e2d1436e8c993c032c726c5
Michael Barton <mail@michaelbarton.me.uk>
2008-05-09 16:09:14 +0100

    Added miltestones attribute to controller.

--- controller/main.rb
+++ controller/main.rb
@@ -5,6 +5,7 @@ class MainController < Ramaze::Controller
     @title = p.title
     @summary = p.html_summary
     @version = p.version
+    @milestones = Milestone.all.sort { |a,b| a.number <=> b.number }
   end
 
   def style
