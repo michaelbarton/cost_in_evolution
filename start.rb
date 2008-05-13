686a285d2015cc15ac31eac8f8fbba9a51d8b3b2
Michael Barton <mail@michaelbarton.me.uk>
2008-05-13 17:40:00 +0100

    Added http redirect option in start.rb file.

--- start.rb
+++ start.rb
@@ -3,4 +3,10 @@ require File.dirname(__FILE__) + '/config/environment.rb'
 config = YAML::load(File.open(File.dirname(__FILE__) + '/config/config.yml'))
 
 Ramaze::Log.loggers << Ramaze::Informer.new(config['www_log'])
+
+if config['path_rewrite']
+  path_re = Regexp.new(config['path_rewrite'])
+  Ramaze::Rewrite[path_re] = "%s"
+end
+
 Ramaze.start config['ramaze']
