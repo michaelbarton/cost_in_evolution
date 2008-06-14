b30337dfcaff58bf4cccc0b4b46f80ed0d0c85f2
Michael Barton <mail@michaelbarton.me.uk>
2008-06-14 15:20:55 +0100

    Added helper methods to generate temporary tree and sequence files for use when running Paml

--- model/evolutionary_rate.rb
+++ model/evolutionary_rate.rb
@@ -7,5 +7,17 @@ class EvolutionaryRate < DataMapper::Base
   property :position,     :integer  # The position in the alignment this refers too
   property :site_rate,    :float    # The evolutionary rate of that position
   property :amino_acids,  :string   # The amino acids at this positon in the alignment
+  
+  def self.store_in_temp_file(alignment)
+    tfile = Tempfile.new('alignment').path
+    File.open(tfile, 'w') {|file| file.puts alignment.to_s}
+    tfile
+  end
+
+  def self.generate_tree_file(alignment)
+    tfile = Tempfile.new('tree').path
+    File.open(tfile, 'w') {|file| file.puts "(#{(1..alignment.gene_count).to_a * ','})"}
+    tfile
+  end
 
 end
