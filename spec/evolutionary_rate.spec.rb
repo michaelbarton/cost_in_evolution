86cad34c7ce28b9621d8ee09f413232d03d5918f
Michael Barton <mail@michaelbarton.me.uk>
2008-06-14 15:23:21 +0100

    Added some simple testing for running codeml analysis using EvolutionaryRate model

---
+++ spec/evolutionary_rate.spec.rb
@@ -0,0 +1,48 @@
+require File.dirname(__FILE__) + '/helper.rb'
+
+valid_gene = File.dirname(__FILE__) + '/data/yal037w.fasta.txt'
+valid_align = File.dirname(__FILE__) + '/data/yal037w.alignment.txt'
+
+describe EvolutionaryRate do
+
+  def clear_tables
+    Gene.delete_all
+    Alignment.delete_all
+    EvolutionaryRate.delete_all
+  end
+
+  before(:each) do
+    clear_tables
+    Gene.create_from_flatfile( Bio::FlatFile.auto(valid_gene).next_entry )
+    Alignment.create_from_alignment(File.open(valid_align).read)
+  end
+
+  after(:each) do
+    clear_tables
+  end
+
+  describe 'Loading alignment into a temporary file' do
+    it 'should load the correct data' do
+      file = EvolutionaryRate.store_in_temp_file(Alignment.all.first)
+      File.open(file).read.should == File.open(valid_align).read
+    end
+  end
+
+  describe 'Creating a temporary tree file' do
+    it 'should generate the expected output' do
+      file = EvolutionaryRate.generate_tree_file(Alignment.all.first)
+      File.open(file).read.strip.should == '(1,2,3)'
+    end
+  end
+
+  describe 'Doing codeml analysis' do
+
+    it 'should not throw an error' do
+      lambda {
+        EvolutionaryRate.codeml_estimate_rate(Alignment.all.first)
+      }.should_not raise_error
+    end
+
+  end
+
+end
