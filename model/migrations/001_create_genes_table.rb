6a9cb62bc9a0eb42bc48efa07c479815e4fb322c
Michael Barton <mail@michaelbarton.me.uk>
2008-06-23 13:07:48 +0100

    Added migrations for alignment and gene

---
+++ model/migrations/001_create_genes_table.rb
@@ -0,0 +1,14 @@
+class CreateGeneTable < ActiveRecord::Migration
+
+  def self.up
+    create_table :genes do |t|
+      t.string :name
+      t.text   :text
+    end
+  end
+
+  def self.down
+    drop_table :genes
+  end
+
+end
---
+++ model/migrations/002_create_alignments_table.rb
@@ -0,0 +1,16 @@
+class CreateAlignmentsTable < ActiveRecord::MigrationA
+
+  def self.up
+    create_table :alignments do |t|
+      t.integer :gene_id
+      t.text    :alignment
+      t.integer :gene_count
+      t.integer :length
+    end
+  end
+
+  def self.down
+    drop_table :alignments
+  end
+
+end
