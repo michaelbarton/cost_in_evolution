c3715a4b1f07ef2bac6a52ac4ffadc893f44da75
Michael Barton <mail@michaelbarton.me.uk>
2008-06-23 16:50:29 +0100

    Added method to populate alignment codons from an alignment.

--- model/alignment_codon.rb
+++ model/alignment_codon.rb
@@ -28,4 +28,37 @@ class AlignmentCodon
     self.amino_acids.sort == expected.sort
   }
 
+  def codons
+    attribute_get(:codons).split(',')
+  end
+ 
+  def codons=(codons)
+    attribute_set(:codons,codons.join(','))
+  end
+
+  def amino_acids
+    attribute_get(:amino_acids).split(',')
+  end
+
+  def amino_acids=(amino_acids)
+    attribute_set(:amino_acids,amino_acids.join(','))
+  end
+
+  def self.create_from_alignment(alignment)
+    alignment.inject(0) do |index, codons|
+      ac = AlignmentCodon.new(
+        :alignment_id   => alignment.id,
+        :start_position => index,
+        :codons         => codons,
+        :amino_acids    => codons.inject(Array.new){ |array,codon| array << Bio::Sequence::NA.new(codon).translate }
+      )
+      if ac.valid?
+        ac.save
+      else
+        ac.errors.each {|error| Needle::Registry.instance[:logger].warn "#{alignment.id}, #{index} : #{error}"}
+      end
+      index + 3
+    end
+  end
+
 end
