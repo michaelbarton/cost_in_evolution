7c06ffe5a1bbf60f3f768203211d2bdf437ff1e8
Michael Barton <mail@michaelbarton.me.uk>
2008-06-23 16:49:16 +0100

    Added additional validations for alignment codon. Validates start_position, codons, and amino_acids

--- model/alignment_codon.rb
+++ model/alignment_codon.rb
@@ -12,8 +12,20 @@ class AlignmentCodon
 
   validates_presence_of :alignment_id, :start_position, :codons, :amino_acids
 
-  validates_true_for :start_postion, :logic => lambda {
-     self.alignment.to_a[self.start_position].sort == self.codons.sort
+  validates_true_for :codons, :logic => lambda {
+     self.alignment != nil and
+       self.alignment.to_a[self.start_position / 3].sort == self.codons.sort
+  }, :message => "Codons should match codons in alignment at start position"
+
+  validates_true_for :start_position, :logic => lambda {
+    self.start_position % 3 == 0
+  }, :message => "Start position should be a multiple of three"
+
+  validates_true_for :amino_acids, :logic => lambda {
+    expected = self.codons.inject(Array.new) do |array,codon|
+      array << Bio::Sequence::NA.new(codon).translate
+    end
+    self.amino_acids.sort == expected.sort
   }
 
 end
