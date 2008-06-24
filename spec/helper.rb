3760efd983708e566ce05c475f5ee5ca622415d1
Michael Barton <mail@michaelbarton.me.uk>
2008-06-24 18:14:03 +0100

    Switched in ActiveRecord code in spec helper

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../config/environment.rb'
 
-DataMapper.setup(:default,Needle::Registry.instance[:database_connections]['testing'])
-DataMapper.auto_migrate!
+ActiveRecord::Base.establish_connection(Needle::Registry.instance.database_connections['testing'])
+[0,nil].each {|i| ActiveRecord::Migrator.migrate(File.dirname(__FILE__) + '/../model/migrations',i)}
 
 Needle::Registry.instance.register(:logger) do
    Logger.new(Needle::Registry.instance.config['log']['testing']) 
