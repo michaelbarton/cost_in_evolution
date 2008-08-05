4e980f6fa67fe548c24480b533a77b937e333bab
Michael Barton <mail@michaelbarton.me.uk>
2008-08-05 17:11:10 +0000

    Added empty specing tasks [#9]

--- spec/evolutionary_rate.spec.rb
+++ spec/evolutionary_rate.spec.rb
@@ -5,7 +5,6 @@ describe EvolutionaryRate do
   before(:each) do
     Gene.create_from_flatfile( Bio::FlatFile.auto(valid_gene).next_entry )
     Alignment.create_from_alignment(File.open(valid_align).read)
-    AlignmentCodons.create_from_alignment(Alignment.first)
   end
 
   after(:each) do
@@ -17,6 +16,45 @@ describe EvolutionaryRate do
     public :initialize
   end
 
+  describe 'Using temporary directory and files' do
 
+    it 'should create the temporary directory' do
+    end
+
+    it 'should create the temporary alignment file' do
+    end
+
+    it 'should create the temporary tree file' do
+    end
+
+    it 'should delete the temporary directory and all files' do
+    end
+
+    it 'should switch working directory to temporary directory' do
+    end
+
+    it 'should switch back temporary directory to working directory'  do
+    end
+
+  end
+
+  describe 'Running estimate evolutionary rate' do
+
+    it 'should run without error' do
+    end
+
+    it 'should return expected gene rate' do
+    end
+
+    it 'should return expected tree length' do
+    end
+
+    it 'should return expect site wise rates' do
+    end
+
+    it 'should clear all files from the temporary directory' do
+    end
+
+  end
 
 end
