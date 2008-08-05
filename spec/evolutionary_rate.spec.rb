f11f31c52615ef8ecc8a106daac2b36cc2ace749
Michael Barton <mail@michaelbarton.me.uk>
2008-08-05 16:55:18 +0000

    Added before and after statements for specing

--- spec/evolutionary_rate.spec.rb
+++ spec/evolutionary_rate.spec.rb
@@ -1,1 +1,22 @@
 require File.dirname(__FILE__) + '/helper.rb'
+
+describe EvolutionaryRate do
+
+  before(:each) do
+    Gene.create_from_flatfile( Bio::FlatFile.auto(valid_gene).next_entry )
+    Alignment.create_from_alignment(File.open(valid_align).read)
+    AlignmentCodons.create_from_alignment(Alignment.first)
+  end
+
+  after(:each) do
+    clear_all_tables
+  end
+
+  # Make private methods public for testing
+  EvolutionaryRate.class_eval do
+    public :initialize
+  end
+
+
+
+end
