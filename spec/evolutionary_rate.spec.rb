ab3841d92c4e065abf4da7500ae704472a887de5
Michael Barton <mail@michaelbarton.me.uk>
2008-08-11 14:28:46 +0000

    Added specing for evolutionary rate results

--- spec/evolutionary_rate.spec.rb
+++ spec/evolutionary_rate.spec.rb
@@ -84,19 +84,55 @@ describe EvolutionaryRate do
   describe 'Running estimate evolutionary rate' do
 
     it 'should run without error' do
+      lambda{
+        EvolutionaryRate.new(Alignment.first).run
+      }.should_not raise_error
     end
 
     it 'should return expected gene rate' do
+      er = EvolutionaryRate.new(Alignment.first)
+      er.run
+      er.gene_rate.should be_close(1.75346, 0.0001)
     end
 
     it 'should return expected tree length' do
+      er = EvolutionaryRate.new(Alignment.first)
+      er.run
+      er.tree_length.should == 0.55773
     end
 
-    it 'should return expect site wise rates' do
+    it 'should return expected number of site wise rates' do
+      er = EvolutionaryRate.new(Alignment.first)
+      er.run
+      er.site_rates.size.should == 263
     end
 
-    it 'should clear all files from the temporary directory' do
+    it 'the first site rate should have the expected results' do
+      er = EvolutionaryRate.new(Alignment.first)
+      er.run
+      position = er.site_rates[1]
+      position[:rate].should == 0.710
+      position[:data].should == "MMM"
     end
+
+    it 'the 100th site rate should have the expected results' do
+      er = EvolutionaryRate.new(Alignment.first)
+      er.run
+      position = er.site_rates[100]
+      position[:rate].should == 1.334
+      position[:data].should == "GGS"
+    end
+
+    it 'the last site rate should have the expected results' do
+      er = EvolutionaryRate.new(Alignment.first)
+      er.run
+      position = er.site_rates[262]
+      position[:rate].should == 0.844
+      position[:data].should == "FFF"
+   end
+
+   it 'should clear all files from the temporary directory' do
+   end
 
   end
 
