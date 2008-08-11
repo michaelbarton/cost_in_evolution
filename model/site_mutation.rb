4858169d8d75b60ab6b3f161ed62db705c11fb11
Michael Barton <mail@michaelbarton.me.uk>
2008-08-11 17:56:02 +0000

    Added helper method to load alignments from rates hash [#9]

--- model/site_mutation.rb
+++ model/site_mutation.rb
@@ -1,3 +1,22 @@
 class SiteMutation < ActiveRecord::Base
   belongs_to :alignment_codon
+
+  def self.create_from_rates(array,alignment)
+
+    codons = alignment.alignment_codons.sort
+
+    array.each do |position|
+      codon = codons.first
+      acids = codon.amino_acids.join
+
+      if position.data == acids
+        SiteMutation.create(
+	  :alignment_codon_id => codon.id,
+	  :rate               => position.rate
+	)
+	# Remove this codon from the queue as it has been assigned
+	codons.shift
+      end
+    end
+  end
 end
