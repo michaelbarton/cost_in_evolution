f9e21cdd876d4523a57bf81e4a967b7bf8702702
Michael Barton <mail@michaelbarton.me.uk>
2008-08-12 13:53:52 +0000

    Added initial specing for site mutation model

--- spec/site_mutation.spec.rb
+++ spec/site_mutation.spec.rb
@@ -1,8 +1,5 @@
 require File.dirname(__FILE__) + '/helper.rb'
 
-valid_gene = File.dirname(__FILE__) + '/data/yal037w.fasta.txt'
-valid_align = File.dirname(__FILE__) + '/data/yal037w.alignment.txt'
-
 describe SiteMutations do
 
   before(:each) do
@@ -15,62 +12,21 @@ describe SiteMutations do
     clear_all_tables
   end
 
-  describe 'Loading alignment into a temporary file' do
-    it 'should load the correct data' do
-      file = EstimateEvolutionaryRate.store_in_temp_file(Alignment.first)
-      File.open(file).read.should == File.open(valid_align).read
-    end
-  end
-
-  describe 'Creating a temporary tree file' do
-    it 'should generate the expected output' do
-      file = EstimateEvolutionaryRate.generate_tree_file(Alignment.first)
-      File.open(file).read.strip.should == '((FYAL037W,PYAL037W)BYAL037W)'
-    end
-  end
-
-  describe 'Running codeml analysis' do
-    it 'should not throw an error' do
+  describe 'loading site mutation rates' do
+    it 'should run without error' do
       lambda {
-        EvolutionaryRate.codeml_estimate_rate(Alignment.all.first)
-      }.should_not raise_error
+        SiteMutation.create_from_rates(EvolutionaryRates.new(Alignment.first).run.site_rates)
+      }.should not_raise
     end
   end
 
-  describe 'Codeml results' do 
+  describe 'the loaded results' do
 
-   before do
-     EvolutionaryRate.codeml_estimate_rate(Alignment.all.first)
-   end
+    SiteMutation.create_from_rates(EvolutionaryRates.new(Alignment.first).run.site_rates)
 
-   it 'should have the expected number of resuts' do
-     EvolutionaryRate.all.length.should == 262
-   end
-
-   it 'should have the correct alignment association' do
-     EvolutionaryRate.first.alignment.should_not == nil
-     EvolutionaryRate.first.alignment.gene.should_not == nil
-     EvolutionaryRate.first.alignment.gene.name.should == "YAL037W"
-   end
-   
-   it 'the first position should have the expected results' do
-     position = EvolutionaryRate.find_by_position(1)
-     position.site_rate.should == 0.710
-     position.amino_acids.should == "MMM"
-   end
-
-   it 'the 100th position should have the expected results' do
-     position = EvolutionaryRate.find_by_position(100)
-     position.site_rate.should == 1.334
-     position.amino_acids.should == "GGS"
-   end
-
-   it 'the last position should have the expected results' do
-     position = EvolutionaryRate.find_by_position(262)
-     position.site_rate.should == 0.844
-     position.amino_acids.should == "FFF"
-   end
-
+    it 'should store the first entry correctly' do
+      sm = AlignmentCodon.find_by_start_position(12).site_mutation
+    end
   end
 
 end
