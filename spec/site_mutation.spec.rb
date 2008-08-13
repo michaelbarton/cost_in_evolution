e88962353eb1025257c3246f2a8454599e73016b
Michael Barton <mail@michaelbarton.me.uk>
2008-08-13 09:59:35 +0000

    Updated typos in sitewise specing [#9]

--- spec/site_mutation.spec.rb
+++ spec/site_mutation.spec.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + '/helper.rb'
 
-describe SiteMutations do
+describe SiteMutation do
 
   before(:each) do
-    Gene.create_from_flatfile( Bio::FlatFile.auto(valid_gene).next_entry )
-    Alignment.create_from_alignment(File.open(valid_align).read)
-    AlignmentCodons.create_from_alignment(Alignment.first)
+    Gene.create_from_flatfile( Bio::FlatFile.auto(GENE).next_entry )
+    Alignment.create_from_alignment(File.open(ALIGN).read)
+    AlignmentCodon.create_from_alignment(Alignment.first)
   end
 
   after(:each) do
@@ -15,17 +15,20 @@ describe SiteMutations do
   describe 'loading site mutation rates' do
     it 'should run without error' do
       lambda {
-        SiteMutation.create_from_rates(EvolutionaryRates.new(Alignment.first).run.site_rates)
+        SiteMutation.create_from_rates(EvolutionaryRate.new(Alignment.first).run.site_rates)
       }.should not_raise
     end
   end
 
   describe 'the loaded results' do
 
-    SiteMutation.create_from_rates(EvolutionaryRates.new(Alignment.first).run.site_rates)
+    before(:each) do
+      SiteMutation.create_from_rates(EvolutionaryRate.new(Alignment.first).run.site_rates)
+    end
 
     it 'should store the first entry correctly' do
       sm = AlignmentCodon.find_by_start_position(12).site_mutation
+      sm.should_not be_nil
     end
   end
 
