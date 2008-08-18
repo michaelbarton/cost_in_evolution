f98cf651f9df106dac67bea00cc682f4a757198c
Michael Barton <mail@michaelbarton.me.uk>
2008-08-18 19:03:28 +0000

    Added method to gene model to strip line breaks from gene sequence. Added additional specing tests.

--- spec/gene.spec.rb
+++ spec/gene.spec.rb
@@ -12,19 +12,30 @@ describe Gene do
 
   describe 'creating a valid gene' do
 
-   it 'should be valid when created with correct data' do
-     gene = Gene.new
-     File.open(GENE) do |file|
-       gene.name = file.readline.strip.split(/\s+/).first.gsub('>','')
-       gene.dna = file.read
-     end
-     gene.valid?.should == true
-   end
+    it 'should be valid when created with correct data' do
+      gene = Gene.new
+      File.open(GENE) do |file|
+        gene.name = file.readline.strip.split(/\s+/).first.gsub('>','')
+        gene.dna = file.read
+      end
+      gene.valid?.should == true
+    end
 
-   it 'should not return nil when created from bioruby entry' do
-     gene = load_gene
-     gene.should_not == nil
-   end
+    it 'should not return nil when created from bioruby entry' do
+      gene = load_gene
+      gene.should_not == nil
+    end
+
+    it 'should have the correct name' do
+      load_gene
+      Gene.first.name.should == 'YDL177C'
+    end
+
+    it 'should store the expected sequence in the database' do
+      load_gene
+      gene = Gene.first
+      gene.dna.should == 'ATGAGTAAGAATGTTGGTAAGCTAGTGAAAATATGGAATGAATCAGAAGTTTTAGTTGATAGAAAATCGAAATTTCAAGCAAGATGTTGCCCATTACAAAATCAAAAGGATATACCCTCCATACTCCAAGAACTAACGCAAAACAACAAAAGCGTCTCCAAGGCATCCCACATGCACATGTATGCCTGGAGAACGGCCGAGGTATCAAATAATTTGCACTTACAACAAGAGCAGAAAAAGAAGGGCAATAAAGCAAATAAGAGTAATAATAGTCATGTTAACAAGTCAAGGAACATAACGGTGCAGCCAAAGAACATTGAGCAAGGATGTGCTGACTGTGGCGAAGCTGGTGCTGGACAGCGTTTATTGACCTTACTTGAAAGAGCAAACATATTCAACGTCTTGGTAATAGTGACCAGATGGTATGGTGGCACGCCTTTGGGCTCATCAAGATTCAGACACATTTCAACATGTGCAGTGGAAACCTTAAAGAAGGGTGGATTTCTTCCTTAA'
+    end
 
   end
 
