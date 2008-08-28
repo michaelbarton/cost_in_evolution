630b96d8aa6678202d07c7d1ac71761410c2186a
Michael Barton <mail@michaelbarton.me.uk>
2008-08-28 16:14:08 +0000

    Added acts_as_summary to gene model

--- model/gene.rb
+++ model/gene.rb
@@ -8,6 +8,10 @@ class Gene < ActiveRecord::Base
   # Checks that the sequence has a start codon, a stop codon, and contains only ATGC
   validates_format_of :dna, :with => /^ATG[ATGC\n]+(TAG|TAA|TGA)$/im
 
+  acts_as_summary :dna,
+    :map   => lambda {|dna| dna.length},
+    :name  => :sequence_length
+
   def self.create_from_flatfile(entry)
     gene = Gene.new
     gene.name = entry.definition.split(/\s+/).first
@@ -19,21 +23,5 @@ class Gene < ActiveRecord::Base
       Needle::Registry.instance[:logger].warn(
        "Load gene: #{gene.name} is not a valid coding ORF")
     end
-  end
-
-  def self.longest
-    Gene.all.collect{|x| x.dna.length}.max
-  end
-
-  def self.shortest
-    Gene.all.collect{|x| x.dna.length}.min
-  end
-
-  def self.mean
-    Gene.all.map{|gene| gene.dna.length}.to_statarray.mean
-  end
-
-  def self.standard_deviation
-    Gene.all.map{|gene| gene.dna.length}.to_statarray.stddev
   end
 end
