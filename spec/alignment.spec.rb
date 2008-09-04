9b9b31054fbe37d85f7137ce51cad7e051627ef7
Michael Barton <mail@michaelbarton.me.uk>
2008-09-04 18:38:56 +0000

    Removed before/after statements from alignment specing. Reordered alignment specing.

--- spec/alignment.spec.rb
+++ spec/alignment.spec.rb
@@ -2,123 +2,117 @@ require File.dirname(__FILE__) + '/helper.rb'
 
 describe Alignment do
 
-  before(:all) do
-    fixtures :genes
-  end
+  fixtures :genes
 
-  after(:each) do
-    Alignment.delete_all
-  end
-
-  describe 'Creating a valid record' do
-
-    before(:each) do
-      @align = Alignment.create_from_alignment(File.open(ALIGN).read)
-    end
-
-    it 'should be valid' do
-      @align.valid?.should == true
-    end
+  describe 'Creating an alignment from file' do
 
     it 'should be saved to the database' do
+      align = Alignment.create_from_alignment(File.open(ALIGN).read)
       Alignment.all.length == 1
+      align.destroy
     end
 
-    it 'should have the correct length' do
-      Alignment.all.length
-      @align.length.should == 606
+    it 'should derive the correct length' do
+      align = Alignment.create_from_alignment(File.open(ALIGN).read)
+      align.length.should == 606
+      align.destroy
     end
 
-    it 'should have the correct gene count' do
-      @align.gene_count.should == 4
+    it 'should derive the correct gene count' do
+      align = Alignment.create_from_alignment(File.open(ALIGN).read)
+      align.gene_count.should == 4
+      align.destroy
     end
 
-    it 'should have the correct tree' do
-      @align.tree.should == "((FYDL177C,PYDL177C)MYDL177C,BYDL177C)"
-    end
-
-    it 'should contain the correct alignment' do
+    it 'should store the correct alignment' do
+      align = Alignment.create_from_alignment(File.open(ALIGN).read)
       File.open(ALIGN) do |f|
         f.readline
-        @align.alignment.should == f.read.strip
+        align.alignment.should == f.read.strip
       end
+      align.destroy
+    end
+
+
+  end
+
+  describe 'A valid alignment model' do
+
+    it 'should return the correct tree' do
+      Factory.build(:alignment).tree.should == "((FYDL177C,PYDL177C)MYDL177C,BYDL177C)"
     end
 
     it 'should return the correct hash map' do
-      @align.sequence_hash.should_not == nil
-      @align.sequence_hash.keys.sort.should == ['FYDL177C','PYDL177C','BYDL177C','MYDL177C'].sort
-      @align.sequence_hash['FYDL177C'].should == '------------------------------------------------------------------------------------ATGAGTAAGAATGTTGGTAAGCTAGTGAAAATATGGAATGAATCAGAAGTTTTAGTTGATAGAAAATCGAAATTTCAAGCAAGATGTTGCCCATTACAAAATCAAAAGGATATACCCTCCATACTCCAAGAACTAACGCAAAACAACAAAAGCGTCTCCAAGGCATCCCACATGCACATGTATGCCTGGAGAACGGCCGAGGTATCAAATAATTTGCACTTACAA---------CAAGAGCAGAAAAAGAAGGGCAATAAAGCAAATAAGAGTAATAATAGT---CATGTTAACAAGTCAAGGAACATAACGGTGCAGCCAAAGAACATTGAGCAAGGATGTGCTGACTGTGGCGAAGCTGGTGCTGGACAGCGTTTATTGACCTTACTTGAAAGAGCAAACATATTCAACGTCTTGGTAATAGTGACCAGATGGTATGGTGGCACGCCTTTGGGCTCATCAAGATTCAGACACATTTCAACATGTGCAGTGGAAACCTTAAAGAAGGGTGGATTTCTTCCT'
+      align = Factory.build(:alignment)
+      align.sequence_hash.should_not == nil
+      align.sequence_hash.keys.sort.should == ['FYDL177C','PYDL177C','BYDL177C','MYDL177C'].sort
+      align.sequence_hash['FYDL177C'].should == '------------------------------------------------------------------------------------ATGAGTAAGAATGTTGGTAAGCTAGTGAAAATATGGAATGAATCAGAAGTTTTAGTTGATAGAAAATCGAAATTTCAAGCAAGATGTTGCCCATTACAAAATCAAAAGGATATACCCTCCATACTCCAAGAACTAACGCAAAACAACAAAAGCGTCTCCAAGGCATCCCACATGCACATGTATGCCTGGAGAACGGCCGAGGTATCAAATAATTTGCACTTACAA---------CAAGAGCAGAAAAAGAAGGGCAATAAAGCAAATAAGAGTAATAATAGT---CATGTTAACAAGTCAAGGAACATAACGGTGCAGCCAAAGAACATTGAGCAAGGATGTGCTGACTGTGGCGAAGCTGGTGCTGGACAGCGTTTATTGACCTTACTTGAAAGAGCAAACATATTCAACGTCTTGGTAATAGTGACCAGATGGTATGGTGGCACGCCTTTGGGCTCATCAAGATTCAGACACATTTCAACATGTGCAGTGGAAACCTTAAAGAAGGGTGGATTTCTTCCT'
     end
 
     it 'should return the correct sequence array' do
-      @align.sequence_array.should_not == nil
-      @align.sequence_array[0][0..9].should == '----------'
-      @align.sequence_array[1][0..9].should == '----------'
-      @align.sequence_array[2][0..9].should == '----------'
-      @align.sequence_array[3][0..9].should == 'ATGCATCACT'
+      align = Factory.build(:alignment)
+      align.sequence_array.should_not == nil
+      align.sequence_array[0][0..9].should == '----------'
+      align.sequence_array[1][0..9].should == '----------'
+      align.sequence_array[2][0..9].should == '----------'
+      align.sequence_array[3][0..9].should == 'ATGCATCACT'
     end
 
-   it 'should be comparable' do
-     (@align.is_a? Enumerable).should == true
-     (@align.respond_to? :each).should == true
-   end
+    it 'should be comparable' do
+      align = Factory.build(:alignment)
+      (align.is_a? Enumerable).should == true
+      (align.respond_to? :each).should == true
+    end
 
-   it 'should implement each correctly' do
-     @align.to_a.first.should == ['---','---','---','ATG']
-     @align.to_a[10].should == ['---','---','---','TTA']
-     @align.to_a[100].should == ['AAT','AAC','CAC','AAC']
-     @align.to_a.last.should == ['CCC','CAT','CCT','CCT']
-   end
+    it 'should implement each correctly' do
+      align = Factory.build(:alignment)
+      align.to_a.first.should == ['---','---','---','ATG']
+      align.to_a[10].should == ['---','---','---','TTA']
+      align.to_a[100].should == ['AAT','AAC','CAC','AAC']
+      align.to_a.last.should == ['CCC','CAT','CCT','CCT']
+    end
 
+    it 'should return to string correctly' do
+      Factory.build(:alignment).to_s.strip.should == File.open(ALIGN).read.strip
+    end
   end
 
   describe 'Creating a non valid record' do
   
-    before do
-      @align = Alignment.new
-      @align.gene_id = 1
-      @align.gene_count = 4
-      @align.length = 4
-    end
-
     it 'should not be valid when alignment field missing' do
-      @align.valid?.should_not == true
-      @align.errors.length.should == 1
-      @align.errors.to_a.first.should == [:alignment, ["can't be empty"]]
+      align = Factory.build(:alignment, :alignment => nil)
+      align.valid?.should_not == true
+      align.errors.length.should == 1
+      align.errors.to_a.first.should == [:alignment, ["can't be empty"]]
     end
 
     it 'should not be valid when count info included' do
-      @align.alignment = "3 10\nFYAL001W ATG..."
-      @align.valid?.should_not == true
-      @align.errors.length.should == 1
-      @align.errors.to_a.first.should == [:alignment, ["Alignment field should not contain alignment count and length"]]
+      align = Factory.build(:alignment, :alignment => "3 10\nFYAL001W ATG...")
+      align.valid?.should_not == true
+      align.errors.length.should == 1
+      align.errors.to_a.first.should == [:alignment, ["Alignment field should not contain alignment count and length"]]
     end
 
     it 'should not be valid when an S. cerevisiae is gene missing from alignment' do
-      @align.alignment = "ATG..."
-      @align.valid?.should_not == true
-      @align.errors.length.should == 1
-      @align.errors.to_a.first.should == [:alignment, ["Alignment should contain an S. cerevisiae gene"]]
+      align = Factory.build(:alignment, :alignment =>  "ATG...")
+      align.valid?.should_not == true
+      align.errors.length.should == 1
+      align.errors.to_a.first.should == [:alignment, ["Alignment should contain an S. cerevisiae gene"]]
     end
 
     it 'should not be valid when there are not four genes' do
-      @align.alignment = 'ATG...'
-      @align.gene_count = 3
-      @align.valid?.should_not == true
-      @align.errors.length.should == 1
-      @align.errors.to_a.first.should == [:gene_count, ["Alignment should contain four genes"]]
+      align = Factory.build(:alignment, :gene_count => 3)
+      align.valid?.should_not == true
+      align.errors.length.should == 1
+      align.errors.to_a.first.should == [:gene_count, ["Alignment should contain four genes"]]
     end
 
     it 'should not be valid when the gene sequence does not match the alignment' do
-
       gene = Gene.create(:name => 'YDL177C', :dna => 'ATGAAATAG')
-      @align.gene_id = gene.id
-      @align.alignment = 'FYDL177C ATG...'
- 
-
-      @align.valid?.should_not == true
-      @align.errors.length.should == 1
-      @align.errors.to_a.first.should == [:gene, ["Alignment sequence should match gene sequence"]]
+      align = Factory.build(:alignment, :gene_id => gene.id, :alignment => 'FYDL177C ATG...')
+      align.valid?.should_not == true
+      align.errors.length.should == 1
+      align.errors.to_a.first.should == [:gene, ["Alignment sequence should match gene sequence"]]
       gene.destroy
     end
   end
@@ -145,18 +139,6 @@ describe Alignment do
       test = "AAA\nBBB\nCCC"
       Alignment.remove_first_line(test).should == "BBB\nCCC"
     end
-  end
-
-  describe 'Alignment to string' do
-
-    before do
-      @align = Alignment.create_from_alignment(File.open(ALIGN).read)
-    end
-
-    it 'should return the alignment in string format' do
-     @align.to_s.strip.should == File.open(ALIGN).read.strip
-    end
-
   end
 
 end
