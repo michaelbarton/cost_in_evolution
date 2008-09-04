00f54a53b892e4071818a6c4828f29904353e1b5
Michael Barton <mail@michaelbarton.me.uk>
2008-09-04 18:49:03 +0000

    Removed before/after statements from alignment codon specing

--- spec/alignment_codon.spec.rb
+++ spec/alignment_codon.spec.rb
@@ -14,14 +14,6 @@ describe AlignmentCodon do
 
   describe 'Creating a single record' do
 
-    after(:each) do
-      AlignmentCodon.delete_all
-    end
-
-    before(:each) do
-      AlignmentCodon.delete_all
-    end
-
     it 'codons method should return an array' do
       Factory.build(:alignment_codon).codons.should == ['---','---','---','ATG']
     end
@@ -43,8 +35,9 @@ describe AlignmentCodon do
     end
 
     it 'should save' do
-      Factory(:alignment_codon)
+      codon = Factory(:alignment_codon)
       AlignmentCodon.all.length.should == 1
+      codon.destroy
     end
 
     it 'using an incorrect position should cause it to be invalid' do
@@ -74,40 +67,27 @@ describe AlignmentCodon do
     end
 
     it 'saving two alignment codons with the same alignment and start position should raise' do
-      Factory(:alignment_codon)
+      codon = Factory(:alignment_codon)
       lambda { Factory(:alignment_codon) }.should raise_error
+      codon.destroy
     end
   end
 
   describe 'Creating a set of alignment codon records' do
 
-    before(:all) do
-      AlignmentCodon.delete_all
-      AlignmentCodon.create_from_alignment(Alignment.first)
-    end
-
-    after(:all) do
-      AlignmentCodon.delete_all
-    end
-
     it 'should create the expected number of records' do
+      AlignmentCodon.create_from_alignment(Alignment.first)
       AlignmentCodon.all.length.should == 202
+      AlignmentCodon.delete_all
     end
 
-    it 'should create the expected first record' do
+    it 'should create the expected records' do
+      AlignmentCodon.create_from_alignment(Alignment.first)
       test_alignment_codon(AlignmentCodon.first,0,['---','---','---','ATG'],['X','X','X','M'],true)
-    end
-
-    it 'should create the expected last record' do
       test_alignment_codon(AlignmentCodon.find_by_start_position(603),603,['CCC','CAT','CCT','CCT'],['P','H','P','P'],false)
-    end
-
-    it 'should create the expected 3rd record' do
       test_alignment_codon(AlignmentCodon.find_by_start_position(9),9,['---','---','---','TTA'],['X','X','X','L'],true)
-    end
-
-    it 'should create the expected 33rd record' do
       test_alignment_codon(AlignmentCodon.find_by_start_position(99),99,['GGG','GGA','GGT','GGC'],['G','G','G','G'],false)
+      AlignmentCodon.delete_all
     end
 
   end
