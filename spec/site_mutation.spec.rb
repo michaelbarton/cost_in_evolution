e61c87f0b698e857554a079de5f1f5421aa60d3e
Michael Barton <mail@michaelbarton.me.uk>
2008-09-04 17:38:34 +0000

    Added new specs for testing site mutation. Also added fixtures.

--- spec/site_mutation.spec.rb
+++ spec/site_mutation.spec.rb
@@ -2,17 +2,36 @@ require File.dirname(__FILE__) + '/helper.rb'
 
 describe SiteMutation do
 
-  before(:each) do
-    load_gene
-    load_align
-    load_align_codons
-  end
+  fixtures :genes, :alignments, :alignment_codons
 
-  after(:each) do
-    clear_all_tables
+  describe 'saving a site mutation rate' do
+  
+    after(:each) do
+      SiteMutation.delete_all
+    end
+
+    it 'should be valid' do
+      Factory.build(:site_mutation).valid?.should == true
+    end
+
+    it 'should not be valid if corresponding alignment codon does not exist' do
+      non_existant_codon_id = 10000
+      Factory.build(:site_mutation, :alignment_codon_id => non_existant_codon_id).valid?.should == false
+    end
+
+    it 'should not save if there is already an existing site mutation with the same alignment codon id' do
+      Factory(:site_mutation)
+      lambda {Factory(:site_mutation)}.should raise_error
+    end
+
   end
 
   describe 'loading site mutation rates' do
+
+    after(:each) do
+      SiteMutation.delete_all
+    end
+
     it 'should run without error' do
       er = EvolutionaryRate.new(Alignment.first).run
       lambda {
@@ -23,9 +42,13 @@ describe SiteMutation do
 
   describe 'the loaded results' do
 
-    before(:each) do
+    before(:all) do
       er = EvolutionaryRate.new(Alignment.first).run
       SiteMutation.create_from_rates(er.site_rates,Alignment.first)
+    end
+
+    after(:all) do
+      SiteMutation.delete_all
     end
 
     it 'should create the expected number of entries' do
