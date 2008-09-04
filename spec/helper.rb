41dc2b2fabf3ed210ba3d1c573244198f51da291
Michael Barton <mail@michaelbarton.me.uk>
2008-09-04 15:24:47 +0000

    Factories loaded in spec helper

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,4 +1,6 @@
 require File.dirname(__FILE__) + '/../config/environment.rb'
+require 'factory_girl'
+Dir.glob(File.dirname(__FILE__) + '/factory/*.rb') {|file| require file}
 
 ActiveRecord::Base.establish_connection(Needle::Registry.instance.database_connections['testing'])
 [0,nil].each {|i| ActiveRecord::Migrator.migrate(File.dirname(__FILE__) + '/../model/migrations',i)}
@@ -16,10 +18,6 @@ ALIGN = File.expand_path(File.dirname(__FILE__) + '/data/ydl177c.alignment.txt')
 def fixtures(*args)
   dir = File.join(File.dirname(__FILE__),'fixtures')
   args.each{|x| Fixtures.create_fixtures(dir,x.to_s) }
-end
-
-def load_align_codons
-  AlignmentCodon.create_from_alignment(Alignment.first)
 end
 
 def estimate_rates
