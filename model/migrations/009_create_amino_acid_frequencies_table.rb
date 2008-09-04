a9dffd406c4773be36329d2da8f8dcfea15846fc
Michael Barton <mail@michaelbarton.me.uk>
2008-09-04 12:21:09 +0000

    Added indicies to amino acid frequencies table

--- model/migrations/009_create_amino_acid_frequencies_table.rb
+++ model/migrations/009_create_amino_acid_frequencies_table.rb
@@ -7,6 +7,10 @@ class CreateAminoAcidFrequenciesTable < ActiveRecord::Migration
       t.float   :frequency
       t.float   :error
     end
+    add_index(:amino_acid_frequencies,:alignment_codon_id, :name => 'codon_index')
+    add_index(:amino_acid_frequencies,:amino_acid_id, :name => 'amino_acid_index')
+    add_index(:amino_acid_frequencies,[:alignment_codon_id,:amino_acid_id],
+      :unique => true, :name => 'codon_amino_acid_index')
   end
 
   def self.down
