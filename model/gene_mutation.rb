d12d76e9c4f8fd89c55cd28fdbd0f05e7ae1fcb8
Michael Barton <mail@michaelbarton.me.uk>
2008-09-15 13:41:59 +0100

    Added acts as summary to gene mutation model

--- model/gene_mutation.rb
+++ model/gene_mutation.rb
@@ -1,3 +1,11 @@
 class GeneMutation < ActiveRecord::Base
   belongs_to :alignment
+
+  acts_as_summary :estimated_rate,
+    :name => :codeml_estimated_rate,
+    :active_record => { :conditions => {:dataset => 'Barton2009'}  }
+
+  acts_as_summary :estimated_rate,
+    :name => :wall_estimated_rate,
+    :active_record => { :conditions => {:dataset => 'Wall2005'}  }
 end
