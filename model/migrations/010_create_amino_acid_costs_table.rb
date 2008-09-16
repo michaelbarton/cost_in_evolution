ada755c4d43b8bc31d51b917032500a0934be2be
Michael Barton <mail@michaelbarton.me.uk>
2008-09-16 19:14:57 +0100

    Renamed problematic type column to name in amino acid cost migration

--- model/migrations/010_create_amino_acid_costs_table.rb
+++ model/migrations/010_create_amino_acid_costs_table.rb
@@ -3,11 +3,11 @@ class CreateAminoAcidCostsTable < ActiveRecord::Migration
   def self.up
     create_table :amino_acid_costs do |t|
       t.integer :amino_acid_id
-      t.string  :type
+      t.string  :name
       t.float   :estimate
     end
     add_index(:amino_acid_costs,:amino_acid_id)
-    add_index(:amino_acid_costs,[:amino_acid_id,:type], :unique => true)
+    add_index(:amino_acid_costs,[:amino_acid_id,:name], :unique => true)
   end
 
   def self.down
