fa202e8711a585198dbe9f27b1992f9048200e15
Michael Barton <mail@michaelbarton.me.uk>
2008-09-22 17:47:29 +0100

    Updated amino acid cost model to reflect updated table

--- model/amino_acid_cost.rb
+++ model/amino_acid_cost.rb
@@ -2,6 +2,8 @@ class AminoAcidCost < ActiveRecord::Base
   include Validatable
 
   belongs_to :amino_acid
+  has_one :condition
+  has_one :cost_type
 
-  validates_presence_of :amino_acid_id, :name, :estimate
+  validates_presence_of :amino_acid_id, :condition_id, :cost_type_id, :estimate
 end
