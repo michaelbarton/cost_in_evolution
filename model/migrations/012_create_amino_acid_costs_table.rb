afe5c782c0e59f984606eb2608e0ed01054c4da3
Michael Barton <mail@michaelbarton.me.uk>
2008-09-22 17:41:33 +0100

    Move amino acid costs migration from 10th to 12th

--- model/migrations/012_create_amino_acid_costs_table.rb
+++ model/migrations/012_create_amino_acid_costs_table.rb
@@ -3,11 +3,12 @@ class CreateAminoAcidCostsTable < ActiveRecord::Migration
   def self.up
     create_table :amino_acid_costs do |t|
       t.integer :amino_acid_id
-      t.string  :name
+      t.integer :condition_id
+      t.integer :cost_type_id
       t.float   :estimate
     end
-    add_index(:amino_acid_costs,:amino_acid_id)
-    add_index(:amino_acid_costs,[:amino_acid_id,:name], :unique => true)
+    add_index(:amino_acid_costs,[:amino_acid_id,:condition_id,:cost_type_id], 
+      :unique => true, :name => :amino_acid_cost_index)
   end
 
   def self.down
