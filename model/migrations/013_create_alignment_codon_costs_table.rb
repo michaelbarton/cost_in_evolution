541acb394190b214b5cde76b96750ffc8a8043b9
Michael Barton <mail@michaelbarton.me.uk>
2008-09-24 18:22:12 +0100

    Added alignment codon cost migration

---
+++ model/migrations/013_create_alignment_codon_costs_table.rb
@@ -0,0 +1,19 @@
+class CreateAlignmentCodonCostsTable < ActiveRecord::Migration
+
+  def self.up
+    create_table :alignment_codon_costs do |t|
+      t.integer :alignment_codon_id
+      t.integer :condition_id
+      t.integer :cost_type_id
+      t.float   :mean
+      t.float   :variance
+    end
+    add_index(:alignment_codon_costs,[:alignment_codon_id,:condition_id,:cost_type_id], 
+      :unique => true, :name => :alignment_codon_cost_index)
+  end
+
+  def self.down
+    drop_table :alignment_codon_costs
+  end
+
+end
