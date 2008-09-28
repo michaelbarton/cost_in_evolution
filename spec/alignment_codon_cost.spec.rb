c0eb5a759746feac82dcfd3d4375cce9343641c7
Michael Barton <mail@michaelbarton.me.uk>
2008-09-28 11:44:35 +0000

    Updated specing for alignment codon cost to degrage gracefully. Removed specing for variance

--- spec/alignment_codon_cost.spec.rb
+++ spec/alignment_codon_cost.spec.rb
@@ -13,9 +13,16 @@ describe AlignmentCodonCost do
       mean.should be_close(4.482759,0.0001)
     end
 
-    it 'should correctly estimate the weighted deviation' do
-      var = AlignmentCodonCost.weighted_variance([4,6,3,6,2],[0.8,0.4,0.4,0.9,0.4])
-      var.should be_close(2.919753,0.0001)
+    it 'should return nil if no observations are passed' do
+      AlignmentCodonCost.weighted_mean([],[]).should be_nil
+    end
+
+    it 'should return nil if the number of observations does not match the number of weights' do
+      AlignmentCodonCost.weighted_mean([4,3,2],[0.1,0.2]).should be_nil
+    end
+
+    it 'should return the first observation if there is only one observation' do
+      AlignmentCodonCost.weighted_mean([4],[0.5]).should == 4
     end
   end
 
@@ -41,21 +48,18 @@ describe AlignmentCodonCost do
 
     it 'should calculate the expected cost for the first codon' do
       AlignmentCodonCost.first.mean.should be_close(149.2, 0.0001)
-      AlignmentCodonCost.first.variance.should be_nil
     end
     
     it 'should calculate the expected cost for codon 174' do
       cost = AlignmentCodon.find_by_start_position(174).
         alignment_codon_costs.select{|x| x.cost_type.abbrv == 'wei'}.first
       cost.mean.should be_close(118.0876, 0.01)
-      cost.variance.should == 8
     end
 
     it 'should calculate the expected cost for codon 201' do
       cost = AlignmentCodon.find_by_start_position(201).
         alignment_codon_costs.select{|x| x.cost_type.abbrv == 'wei'}.first
       cost.mean.should be_close(135.1199, 0.01)
-      cost.variance.should be_close( 1806.005,0.01)
     end
   end
 
