1f4d37d77e073e8350b44d5bfc033c065cfd2c81
Michael Barton <mail@michaelbarton.me.uk>
2008-10-01 18:17:46 +0100

    Script to plot average protein cost dataset

---
+++ analysis/003_estimate_amino_acid_frequency_and_cost/r/scripts/plot_gene_costs_density.R
@@ -0,0 +1,24 @@
+rm(list=ls())
+library(lattice)
+
+file = paste(getwd(),'../data/average_protein_costs.csv',sep='/')
+data = read.csv(file)
+#data$cost_type <- levels(data$cost_type)
+
+levels(data$cost_type)[which(levels(data$cost_type)  == 'nit-rel')] <- "Nitrogen Relative"
+levels(data$cost_type)[which(levels(data$cost_type)  == 'nit-abs')] <- "Nitrogen Absolute"
+levels(data$cost_type)[which(levels(data$cost_type)  == 'car-rel')] <- "Carbon Relative"
+levels(data$cost_type)[which(levels(data$cost_type)  == 'car-abs')] <- "Carbon Absolute"
+levels(data$cost_type)[which(levels(data$cost_type)  == 'sul-rel')] <- "Sulphur Relative"
+levels(data$cost_type)[which(levels(data$cost_type)  == 'sul-abs')] <- "Sulphur Absolute"
+levels(data$cost_type)[which(levels(data$cost_type)  == 'phos-rel')] <- "Phosphorus Relative"
+levels(data$cost_type)[which(levels(data$cost_type)  == 'phos-abs')] <- "Phosphorus Absolute"
+levels(data$cost_type)[which(levels(data$cost_type)  == 'none-wei')] <- "Molecular Weight"
+
+
+chart <- densityplot(
+  ~ cost | cost_type,
+  data=data,
+  xlab="Frequency weighted, mean per residue protein cost",
+  scale="free")
+
