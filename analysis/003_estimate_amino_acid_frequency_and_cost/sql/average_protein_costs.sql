0682a5e0cc5810b04481017a4cdffb7515b3c279
Michael Barton <mail@michaelbarton.me.uk>
2008-10-01 18:16:15 +0100

    SQL file to pull average protein cost

---
+++ analysis/003_estimate_amino_acid_frequency_and_cost/sql/average_protein_costs.sql
@@ -0,0 +1,17 @@
+SELECT
+  genes.name AS gene,
+  CONCAT(conditions.abbrv, '-', cost_types.abbrv) AS cost_type,
+  AVG(alignment_codon_costs.mean) AS cost
+FROM
+  alignment_codon_costs
+LEFT JOIN
+  conditions ON alignment_codon_costs.condition_id = conditions.id
+LEFT JOIN
+  cost_types ON alignment_codon_costs.cost_type_id = cost_types.id
+LEFT JOIN
+  alignment_codons  ON alignment_codon_costs.alignment_codon_id = alignment_codons.id
+LEFT JOIN
+  alignments ON alignment_codons.alignment_id = alignments.id
+LEFT JOIN
+  genes ON alignments.gene_id = genes.id
+GROUP BY genes.id, conditions.id, cost_types.id
