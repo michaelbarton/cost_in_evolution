3dadc0814f788933aad71d50e5c3e99241b3bfd8
Michael Barton <mail@michaelbarton.me.uk>
2008-10-02 14:43:30 +0000

    Alignment codon starfish loader uses Alignment as queue rather than alignment codon

--- model/starfish/load_alignment_codon_costs.rb
+++ model/starfish/load_alignment_codon_costs.rb
@@ -1,10 +1,12 @@
 require File.dirname(__FILE__) + '/../../config/environment.rb'
 
 server do |map_reduce|
-  map_reduce.type = AlignmentCodon
-  map_reduce.queue_size = 1000
+  map_reduce.type = Alignment
+  map_reduce.queue_size = 500
 end
 
-client do |codon|
-  AlignmentCodonCost.create_from_codon(codon)
+client do |align|
+  align.alignment_codons.each do |codon|
+    AlignmentCodonCost.create_from_codon(codon)
+  end
 end
