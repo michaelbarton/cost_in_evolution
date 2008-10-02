09a30bbca68c1ff4e57c0718f6552572d6da5aaa
Michael Barton <mail@michaelbarton.me.uk>
2008-10-02 14:44:26 +0000

    Updated output_from_sql, no longer stores entire dataset in memory

--- helpers/output_from_sql.rb
+++ helpers/output_from_sql.rb
@@ -4,13 +4,11 @@ def output_from_sql(sql_file,target_file)
 
   raise ArgumentError, "SQL file not found : #{sql_file}" unless File.exists?(sql_file)
 
-  data = ActiveRecord::Base.connection.select_all(File.open(sql_file).read)
+  data = ActiveRecord::Base.connection.execute(File.open(sql_file).read)
 
-  keys = data.first.keys
+  keys = data.fetch_fields.map(&:name)
   FasterCSV.open(target_file,'w') do |csv|
     csv << keys
-    data.each do |row|
-      csv << keys.map {|key| row[key]}
-    end
+    data.each { |row| csv << row }
   end
 end
