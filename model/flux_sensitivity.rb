be8afb13480dd02d901312654add2ce5d9181bac
Michael Barton <mail@michaelbarton.me.uk>
2008-10-20 15:31:03 +0100

    Removed validation for gene_id to be present. May be nil if no corresponding verified ORF

--- model/flux_sensitivity.rb
+++ model/flux_sensitivity.rb
@@ -4,6 +4,9 @@ class FluxSensitivity < ActiveRecord::Base
   belongs_to :gene
   belongs_to :condition
   belongs_to :cost_type
+  
+  # Gene_id may be nil since there may be reactions for which there is no gene product
+  # i.e. the listed ORF is not one verified by SGD
+  validates_presence_of :condition_id, :cost_type_id, :reaction_name, :estimate
 
-  validates_presence_of :gene_id, :condition_id, :cost_type_id, :reaction_name, :estimate
 end
