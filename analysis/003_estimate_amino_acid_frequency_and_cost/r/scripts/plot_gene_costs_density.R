284e95a4eccf4eaba722ab8d53f1296b0ead8007
Michael Barton <mail@michaelbarton.me.uk>
2008-12-03 16:29:10 +0000

    Created script to plot gene cost densities

---
+++ analysis/003_estimate_amino_acid_frequency_and_cost/r/scripts/plot_gene_costs_density.R
@@ -0,0 +1,31 @@
+rm(list=ls())
+library(lattice)
+library(reshape)
+
+file = paste(getwd(),'../data/gene_costs.csv',sep='/')
+data = read.csv(file)
+
+melted = melt(data,id.vars=c('gene','alignment'))
+
+chart <- densityplot(
+  ~ value | variable,
+  scale='free',
+  data=melted,
+  xlab="Protein weight (Da)",
+  panel=function(x,subscripts){
+    subset <- melted[subscripts,]
+    values <- c('true','false')
+    colours <- c('red','blue')
+    for(i in 1:2){
+      panel.densityplot(
+        subset[subset$alignment == values[i],]$value,
+        col=colours[i]
+      )
+    }
+  }
+)
+
+postscript(paste(getwd(),'../plots/average_gene_cost_density.eps',sep='/'),
+  width=10,height=10,onefile=FALSE,horizontal=FALSE, paper = "special")
+print(chart)
+graphics.off()
