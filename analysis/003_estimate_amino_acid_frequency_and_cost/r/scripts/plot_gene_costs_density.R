f3bc3444fccf14a52fb64e17c5c1e87333597005
Michael Barton <mail@michaelbarton.me.uk>
2008-12-04 17:16:41 +0000

    Updated plotting of gene cost density. More pretty.

--- analysis/003_estimate_amino_acid_frequency_and_cost/r/scripts/plot_gene_costs_density.R
+++ analysis/003_estimate_amino_acid_frequency_and_cost/r/scripts/plot_gene_costs_density.R
@@ -19,13 +19,14 @@ chart <- densityplot(
     for(i in 1:2){
       panel.densityplot(
         subset[subset$alignment == values[i],]$value,
-        col=colours[i]
+        col=colours[i],
+        pch='.'
       )
     }
   }
 )
 
 postscript(paste(getwd(),'../plots/average_gene_cost_density.eps',sep='/'),
-  width=10,height=10,onefile=FALSE,horizontal=FALSE, paper = "special")
+  width=10,height=5,onefile=FALSE,horizontal=FALSE, paper = "special")
 print(chart)
 graphics.off()
